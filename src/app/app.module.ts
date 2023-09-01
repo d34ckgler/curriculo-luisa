a6ebfe4461b29d692e100fb7886fe2b696d54de2
d34ckgler <54666470+d34ckgler@users.noreply.github.com>
2023-09-01 18:51:25 -0400

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  imports: [BrowserAnimationsModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
